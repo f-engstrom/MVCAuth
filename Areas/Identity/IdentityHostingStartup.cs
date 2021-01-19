4c0a3bb0ad67fc759001b872b74b7e5c70960706
Frans Engström <frans.engstrom@learnet.se>
2021-01-19 12:02:28 +0100

    first commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCAuth.Models;
+
+[assembly: HostingStartup(typeof(MVCAuth.Areas.Identity.IdentityHostingStartup))]
+namespace MVCAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MVCAuthContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MVCAuthContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MVCAuthContext>();
+            });
+        }
+    }
+}
